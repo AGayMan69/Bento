0ca2c95a7aed4a207e5bdf312a5bf7242677e892
AGayMan69 <69163298+AGayMan69@users.noreply.github.com>
2021-06-18 22:33:40 +0800

    Update greeting.js
    🤪🤪🤪

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' 🤪🤪';
 
 // Here you can change your greetings
 const gree1 = 'Go to Sleep!  ';
